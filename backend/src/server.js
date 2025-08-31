3a9245e7d2b9b0b50f669f124b148bb39bf72cfc
Arijit Mondal <arijitm717@gmail.com>
2025-08-31 23:29:04 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -57,7 +57,7 @@ app.use("/api/admin", adminRouter);
 // Serving frontend
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (_, res) => {
+  app.get("/*path", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
